44073bafa21208f21cb26f68245adef3e26b34c5
Shgck <shgck@pistache.land>
2015-08-30 19:20:42 +0200

    Init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name = "Python Shgck Tools",
+    version = "1.0",
+    description = "Some utilities I use on various Python projects",
+    author = "Shgck",
+    author_email = "shgck@pistache.land",
+    url = "https://gitlab.com/Shgck/py-shgck-tools",
+
+    packages = ["shgck_tools"]
+)
