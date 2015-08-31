0dc6ce790bd699748966276831b6b4ce53456ebe
Shgck <shgck@pistache.land>
2015-08-31 12:58:11 +0200

    UTF16 mode for read_cstring

--- shgck_tools/bin.py
+++ shgck_tools/bin.py
@@ -1,17 +1,26 @@
 """ Utilities for parsing binary data. """
 
 
-def read_cstring(data, offset):
+def read_cstring(data, offset, utf16_mode = False):
     """ Read the 0-terminated string from data at position offset and return
-    corresponding bytes object (terminator excluded). """
+    corresponding bytes object (terminator excluded).
+
+    If utf16_mode is true, it doesn't stop reading on the null bytes caused by
+    smaller Unicode code points. In little-endian, that's just checking for an
+    even offset to stop. It works, r-right?
+    """
     string_bytes = b""
+    current_offset = offset
     while True:
-        data.seek(offset)
+        data.seek(current_offset)
         next_byte = data.read(1)
-        if not next_byte or next_byte == b"\x00":
+        if not next_byte:
             break
+        if next_byte == b"\x00":
+            if not (utf16_mode and (current_offset - offset) % 2 != 0):
+                break
         string_bytes += next_byte
-        offset += 1
+        current_offset += 1
     return string_bytes
 
 def read_struct(file_object, struct):
