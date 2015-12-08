fcb9f01e947949f11f4a1dde90641309b04f7b6b
Shgck <shgck@pistache.land>
2015-12-08 15:11:52 +0100

    A tiny concurrency helper module

---
+++ pyshgck/concurrency.py
@@ -0,0 +1,10 @@
+""" Helpers for concurrency and threading stuff. """
+
+import threading
+
+
+def simple_thread(func):
+    """ Start function in another thread, discarding return value. """
+    thread = threading.Thread(target = func)
+    thread.daemon = True
+    thread.start()
