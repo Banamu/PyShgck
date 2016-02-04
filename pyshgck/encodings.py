de274dfd3e932f326aed6fd6742d9bb739a1a6c3
Shgck <shgck@pistache.land>
2016-02-04 07:30:18 +0100

    Clean encodings module
    Use the standard dict to get all the encodings names.

--- pyshgck/encodings.py
+++ pyshgck/encodings.py
@@ -1,64 +1,47 @@
 """ A standalone tool to try all Python encodings on a text file. """
 
 import argparse
+import codecs
+import encodings.aliases
 import sys
 
 
 DESCRIPTION = "Try various encodings for this text file."
 LOGFILE_ARG_HELP = "log file, use - to output on stdout"
 
-ENCODINGS = [
-  "ascii", "big5", "big5hkscs",
-  "cp037", "cp273", "cp424", "cp437", "cp500", "cp720", "cp737", "cp775",
-  "cp850", "cp852", "cp855", "cp856", "cp857", "cp858", "cp860", "cp861",
-  "cp862", "cp863", "cp864", "cp865", "cp866", "cp869", "cp874", "cp875",
-  "cp932", "cp949", "cp950", "cp1006", "cp1026", "cp1125", "cp1140", "cp1250",
-  "cp1251", "cp1252", "cp1253", "cp1254", "cp1255", "cp1256", "cp1257",
-  "cp1258", "cp65001",
-  "euc_jp", "euc_jis_2004", "euc_jisx0213", "euc_kr",
-  "gb2312", "gbk", "gb18030", "hz",
-  "iso2022_jp", "iso2022_jp_1", "iso2022_jp_2", "iso2022_jp_2004",
-  "iso2022_jp_3", "iso2022_jp_ext", "iso2022_kr",
-  "latin_1",
-  "iso8859_2", "iso8859_3", "iso8859_4", "iso8859_5", "iso8859_6", "iso8859_7",
-  "iso8859_8", "iso8859_9", "iso8859_10", "iso8859_13", "iso8859_14",
-  "iso8859_15", "iso8859_16",
-  "johab", "koi8_r", "koi8_u",
-  "mac_cyrillic", "mac_greek", "mac_iceland", "mac_latin2",
-  "mac_roman", "mac_turkish",
-  "ptcp154",
-  "shift_jis", "shift_jis_2004", "shift_jisx0213",
-  "utf_32", "utf_32_be", "utf_32_le", "utf_16", "utf_16_be", "utf_16_le",
-  "utf_7", "utf_8", "utf_8_sig"
-]
+# Get all the encodings names directly from the standard aliases dict, but
+# remove special irrelevant encodings that will raise specific exceptions.
+ENCODINGS = set(encodings.aliases.aliases.values())
+ENCODINGS -= set( ( "base64_codec", "bz2_codec", "hex_codec", "rot_13"
+                  , "tactis", "uu_codec", "zlib_codec" ) )
 
 
 def try_encodings(data, log_file):
-    """ Try to decode data for all known encodings, store output in a file named
-    log_file.
-
-    If log_file is the string '-', the output is printed on stdout. """
+    """ Try to decode data for all known encodings (see codecs module doc),
+    store output in a file named log_file, or stdout if log_file is '-'. """
     results = {}
     for encoding in ENCODINGS:
         results[encoding] = try_encoding(encoding, data)
 
     if log_file != "-":
         with open(log_file, "w", encoding = "utf8") as log:
-            write_log(results, log)
+            _write_log(results, log)
     else:
-        write_log(results, sys.stdout)
+        _write_log(results, sys.stdout)
 
 def try_encoding(encoding, data):
+    """ Return decoded data, or None if decoding failed. """
     try:
-        decoded = data.decode(encoding = encoding)
-        return decoded
+        print(data)
+        return codecs.decode(data, encoding, "strict")
     except UnicodeDecodeError:
         return None
 
-def write_log(results, logger):
+
+def _write_log(results, logger):
     for encoding in results:
         result = results[encoding] or "<decoding failed>"
-        logger.write(encoding + ": " + result + "\n")
+        logger.write("{:<15} -> {}\n".format(encoding, result))
 
 
 def main():
