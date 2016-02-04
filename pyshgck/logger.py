cd5ad64393b3af25f2626b192d0aac6475560bf6
Shgck <shgck@pistache.land>
2016-02-04 10:07:42 +0100

    Better logger system (Windows only for now)

--- pyshgck/logger.py
+++ pyshgck/logger.py
@@ -1,112 +1,143 @@
-""" A colored root logger (http://stackoverflow.com/a/1336640) """
+""" A colored stream/file logger. """
 
 import ctypes
+import ctypes.util
 import logging
 import platform
 
 
+# class _AnsiColorStreamHandler(logging.StreamHandler):
+#     DEFAULT = '\x1b[0m'
+#     RED     = '\x1b[31m'
+#     GREEN   = '\x1b[32m'
+#     YELLOW  = '\x1b[33m'
+#     CYAN    = '\x1b[36m'
+
+#     CRITICAL = RED
+#     ERROR    = RED
+#     WARNING  = YELLOW
+#     INFO     = GREEN
+#     DEBUG    = CYAN
+
+#     @classmethod
+#     def _get_color(cls, level):
+#         if level >= logging.CRITICAL:  return cls.CRITICAL
+#         elif level >= logging.ERROR:   return cls.ERROR
+#         elif level >= logging.WARNING: return cls.WARNING
+#         elif level >= logging.INFO:    return cls.INFO
+#         elif level >= logging.DEBUG:   return cls.DEBUG
+#         else:                          return cls.DEFAULT
+
+#     def __init__(self, stream=None):
+#         logging.StreamHandler.__init__(self, stream)
+
+#     def format(self, record):
+#         text = logging.StreamHandler.format(self, record)
+#         color = self._get_color(record.levelno)
+#         return color + text + self.DEFAULT
+
+# Disable protected member access warning for MSVC functions.
+# pylint: disable=W0212
+class _WinColorStreamHandler(logging.StreamHandler):
+
+    STD_INPUT_HANDLE     = -10
+    STD_OUTPUT_HANDLE    = -11
+    STD_ERROR_HANDLE     = -12
+
+    FOREGROUND_BLACK     = 0x0000
+    FOREGROUND_BLUE      = 0x0001
+    FOREGROUND_GREEN     = 0x0002
+    FOREGROUND_CYAN      = 0x0003
+    FOREGROUND_RED       = 0x0004
+    FOREGROUND_MAGENTA   = 0x0005
+    FOREGROUND_YELLOW    = 0x0006
+    FOREGROUND_GREY      = 0x0007
+    FOREGROUND_INTENSITY = 0x0008
+    FOREGROUND_WHITE     = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+
+    BACKGROUND_BLACK     = 0x0000
+    BACKGROUND_BLUE      = 0x0010
+    BACKGROUND_GREEN     = 0x0020
+    BACKGROUND_CYAN      = 0x0030
+    BACKGROUND_RED       = 0x0040
+    BACKGROUND_MAGENTA   = 0x0050
+    BACKGROUND_YELLOW    = 0x0060
+    BACKGROUND_GREY      = 0x0070
+    BACKGROUND_INTENSITY = 0x0080
+
+    DEFAULT  = FOREGROUND_WHITE
+    CRITICAL = FOREGROUND_RED | FOREGROUND_INTENSITY
+    ERROR    = FOREGROUND_RED | FOREGROUND_INTENSITY
+    WARNING  = FOREGROUND_YELLOW | FOREGROUND_INTENSITY
+    INFO     = FOREGROUND_GREEN
+    DEBUG    = FOREGROUND_CYAN
+
+    def __init__(self, stream = None):
+        super().__init__(stream)
+        self.output_handle = self._get_output_handle(stream)
+
+    @classmethod
+    def _get_output_handle(cls, stream):
+        if stream is None:
+            return ctypes.windll.kernel32.GetStdHandle(cls.STD_OUTPUT_HANDLE)
+        else:
+            msvcrt_loc = ctypes.util.find_msvcrt()
+            msvcrt_lib = ctypes.cdll.LoadLibrary(msvcrt_loc)
+            return msvcrt_lib._get_osfhandle(stream.fileno())
+
+    def emit(self, record):
+        color_code = self._get_color_code(record.levelno)
+        self._set_color_code(color_code)
+        super().emit(record)
+        self._set_color_code(self.FOREGROUND_WHITE)
+
+    @classmethod
+    def _get_color_code(cls, level):
+        if level >= logging.CRITICAL:
+            return cls.CRITICAL
+        elif level >= logging.ERROR:
+            return cls.ERROR
+        elif level >= logging.WARNING:
+            return cls.WARNING
+        elif level >= logging.INFO:
+            return cls.INFO
+        elif level >= logging.DEBUG:
+            return cls.DEBUG
+        else:
+            return cls.DEFAULT
+
+    def _set_color_code(self, code):
+        ctypes.windll.kernel32.SetConsoleTextAttribute(self.output_handle, code)
+
+# select ColorStreamHandler based on platform
+if platform.system() == "Windows":
+    ColorStreamHandler = _WinColorStreamHandler
+# else:
+#     ColorStreamHandler = _AnsiColorStreamHandler
+
+
+_LOG_LEVEL   = logging.DEBUG
 _FORMAT      = "%(asctime)s %(levelname)-8s %(message)s"
 _DATE_FORMAT = "%H:%M:%S"
-_LOG_LEVEL   = logging.DEBUG
-
-_STD_INPUT_HANDLE     = -10
-_STD_OUTPUT_HANDLE    = -11
-_STD_ERROR_HANDLE     = -12
-
-_FOREGROUND_BLACK     = 0x0000
-_FOREGROUND_BLUE      = 0x0001
-_FOREGROUND_GREEN     = 0x0002
-_FOREGROUND_CYAN      = 0x0003
-_FOREGROUND_RED       = 0x0004
-_FOREGROUND_MAGENTA   = 0x0005
-_FOREGROUND_YELLOW    = 0x0006
-_FOREGROUND_GREY      = 0x0007
-_FOREGROUND_INTENSITY = 0x0008
-_FOREGROUND_WHITE     = _FOREGROUND_BLUE | _FOREGROUND_GREEN | _FOREGROUND_RED
-
-_BACKGROUND_BLACK     = 0x0000
-_BACKGROUND_BLUE      = 0x0010
-_BACKGROUND_GREEN     = 0x0020
-_BACKGROUND_CYAN      = 0x0030
-_BACKGROUND_RED       = 0x0040
-_BACKGROUND_MAGENTA   = 0x0050
-_BACKGROUND_YELLOW    = 0x0060
-_BACKGROUND_GREY      = 0x0070
-_BACKGROUND_INTENSITY = 0x0080
 
 
-def _add_coloring_to_emit_windows(func):
+def get_logger( name = "pyshgck", level = _LOG_LEVEL
+              , log_format = _FORMAT, date_format = _DATE_FORMAT
+              , into_stderr = True, into_log_file = None ):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(fmt = log_format, datefmt = date_format)
 
-    def _out_handle(self):
-        return ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
-    out_handle = property(_out_handle)
+    if into_stderr:
+        stream_handler = ColorStreamHandler()
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
 
-    def _set_color(self, code):
-        self.STD_OUTPUT_HANDLE = -11
-        hdl = ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
-        ctypes.windll.kernel32.SetConsoleTextAttribute(hdl, code)
-
-    setattr(logging.StreamHandler, '_set_color', _set_color)
-
-    def new(*args):
-        levelno = args[1].levelno
-        if levelno >= 50:
-            color = _FOREGROUND_RED | _FOREGROUND_INTENSITY
-        elif levelno >= 40:
-            color = _FOREGROUND_RED | _FOREGROUND_INTENSITY
-        elif levelno >= 30:
-            color = _FOREGROUND_YELLOW | _FOREGROUND_INTENSITY
-        elif levelno >= 20:
-            color = _FOREGROUND_WHITE
-        elif levelno >= 10:
-            color = _FOREGROUND_CYAN
-        else:
-            color = _FOREGROUND_WHITE
-
-        args[0]._set_color(color)
-        ret = func(*args)
-        args[0]._set_color(_FOREGROUND_WHITE)
-        return ret
-    return new
-
-def _add_coloring_to_emit_ansi(func):
-
-    black, red, green, yellow, blue, magenta, cyan, white = range(8)
-
-    def new(*args):
-        levelno = args[1].levelno
-        if levelno >= 50:
-            color = '\x1b[31m'
-        elif levelno >= 40:
-            color = '\x1b[31m'
-        elif levelno >= 30:
-            color = '\x1b[33m'
-        elif levelno >= 20:
-            color = '\x1b[37m'
-        elif levelno >= 10:
-            color = '\x1b[36m'
-        else:
-            color = '\x1b[0m'
-        args[1].msg = color + args[1].msg +  '\x1b[0m'
-
-        return func(*args)
-    return new
-
-def _get_logger():
-    logging.basicConfig(level = _LOG_LEVEL, format = _FORMAT,
-        datefmt = _DATE_FORMAT)
-    logger = logging.getLogger()
-
-    _patch_logging_stream_handler()
+    if into_log_file is not None:
+        file_handler = logging.FileHandler(into_log_file, mode = "w")
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     return logger
-
-def _patch_logging_stream_handler():
-    if platform.system() == "Windows":
-        coloring_func = _add_coloring_to_emit_windows
-    else:
-        coloring_func = _add_coloring_to_emit_ansi
-    logging.StreamHandler.emit = coloring_func(logging.StreamHandler.emit)
-
-
-LOG = _get_logger()
