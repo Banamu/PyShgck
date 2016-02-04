c9bb7f9c99856541820dbc9a2fe9a372b9a6ce6e
Shgck <shgck@pistache.land>
2016-02-04 10:08:22 +0100

    Fix bad test class name

--- tests/test_encodings.py
+++ tests/test_encodings.py
@@ -11,9 +11,10 @@ FILES_DIR = join(dirname(__file__), "files")
 DUMMY_TEXT_FILE_PATH = join(FILES_DIR, "dummy.txt")
 
 
-class BinTests(unittest.TestCase):
+class EncodingsTests(unittest.TestCase):
 
     def test_try_encodings(self):
+        # Just ensure that it doesn't raise exceptions, that's all I need.
         with open(DUMMY_TEXT_FILE_PATH, "rb") as dummy_text_file:
             binary_text = dummy_text_file.read()
             try_encodings(binary_text, '-')
