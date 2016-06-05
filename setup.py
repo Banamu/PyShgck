980fe8eeb4ec0096d8d4f5f62bb5f0a8f60b6833
Shgck <shgck@pistache.land>
2016-06-05 12:42:33 +0200

    Version 2.0.0

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 
 
 setup(
-    name = "PyShgck",
-    version = "1.1.0",
-    description = "Toolbox with utilities I use on various Python projects",
-    author = "Shgck",
+    name         = "PyShgck",
+    version      = "2.0.0",
+    description  = "Toolbox with utilities I use on various Python projects",
+    author       = "Shgck",
     author_email = "shgck@pistache.land",
-    url = "https://gitlab.com/Shgck/py-shgck-tools",
-
-    packages = ["pyshgck"]
+    url          = "https://gitlab.com/Shgck/py-shgck-tools",
+    packages     = ["pyshgck"]
 )
-
